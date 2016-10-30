18f633318ddeee84dbdd9b9b22a37c82da181a07
mogh <mogh@example.com>
2016-10-30 18:12:59 -0400

    completed interests model

--- app/models/interest.rb
+++ app/models/interest.rb
@@ -1,2 +1,12 @@
 class Interest < ActiveRecord::Base
+
+	# Relationships
+  belongs_to :student
+
+  # Scopes
+  scope :alphabetical,  -> { order(:name) }
+
+  #Validations
+  validates_presence_of :name
+
 end
