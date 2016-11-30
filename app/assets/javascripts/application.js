6ff33e025586eace8135f98f5bf64edeeb61f682
mogh <mogh@example.com>
2016-11-29 22:39:54 -0800

    fixed forms with new gem

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,28 +11,29 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.turbolinks
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
 
-$(document).ready(ready)
-$(document).on('page:load', ready)
+$( document ).on('turbolinks:load', function() {
 
-$(function(){
-    $(function(){
-      $('.dropdown-button').dropdown({
-      inDuration: 300,
-      outDuration: 225,
-      constrain_width: false, // Does not change width of dropdown to that of the activator
-      hover: true, // Activate on hover
-      gutter: 0, // Spacing from edge
-      belowOrigin: true // Displays dropdown below the button
-    }
-  );
+  $(function(){
+      $(function(){
+        $('.dropdown-button').dropdown({
+        inDuration: 300,
+        outDuration: 225,
+        constrain_width: false, // Does not change width of dropdown to that of the activator
+        hover: true, // Activate on hover
+        gutter: 0, // Spacing from edge
+        belowOrigin: true // Displays dropdown below the button
+      }
+    );
+      });
     });
-  });
 
-$(document).ready(function() {
-  $('select').material_select();
+  $(document).ready(function() {
+    $('select').material_select();
+  });
 
 });
